c99c396ad225af1f6963ae810c8fe97a15dcf5b8
jaivik13 <jaivik.r.patel@gmail.com>
2020-01-25 22:52:26 +1000

    Update logic_test.php

--- logic_test.php
+++ logic_test.php
@@ -1,12 +1,12 @@
 <?php
 $var = range(0, 100);
-foreach ($var as &$number) {
-  if ($number % 3 == 0 && $number % 5 == 0) {
-    echo ">>>>>>>  foobar \n \n";
-  } elseif ($number % 3 == 0) {
-    echo ">>>>>>>  foo \n \n";
-  } elseif ($number % 5 == 0) {
-    echo ">>>>>>>  bar \n \n";
-  } else
-    echo "Numbers : " . $number . "\n \n";
-}
+  foreach ($var as &$number) {
+    if ($number % 3 == 0 && $number % 5 == 0) {
+      echo ">>>>>>>  foobar \n \n";
+    } elseif ($number % 3 == 0) {
+      echo ">>>>>>>  foo \n \n";
+    } elseif ($number % 5 == 0) {
+      echo ">>>>>>>  bar \n \n";
+    } else
+      echo "Numbers : " . $number . "\n \n";
+  }
